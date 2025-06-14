b964159c163ea15a3cd66777000ec85cc7997737
Alexrior <sasadorodnikov39@gmail.com>
2025-06-14 14:45:40 +0500

    Add files via upload

---
+++ java_ex/lab9/task8/Node.java
@@ -0,0 +1,11 @@
+package lab9.task08;
+
+public class Node {
+    public String value;
+    public Node next;
+
+    Node(String value) {
+        this.value = value;
+        this.next = null;
+    }
+}
